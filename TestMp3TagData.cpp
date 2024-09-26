6c57b013870074f00d450868d695ae046eb1c46c
pkisensee <pkisensee@msn.com>
2024-09-26 09:49:25 -0700

    Frame -> ID3Frame

--- TestMp3TagData.cpp
+++ TestMp3TagData.cpp
@@ -43,18 +43,18 @@ void TestBaseTagData()
 
   uint8_t zero = 0;
   uint8_t badFrameHdr[ 5 ] = "0aX:";
-  test( !Mp3BaseTagData::IsValidFrame( nullptr ) );
-  test( !Mp3BaseTagData::IsValidFrame( &zero ) );
-  test( !Mp3BaseTagData::IsValidFrame( badFrameHdr ) );
-  test( !Mp3BaseTagData::IsValidFrameID( "tooLong" ) );
-  test( !Mp3BaseTagData::IsValidFrameID( "sm" ) );
-  test( Mp3BaseTagData::IsValidFrameID( "TPE1" ) );
+  test( !Mp3BaseTagData::IsValidID3Frame( nullptr ) );
+  test( !Mp3BaseTagData::IsValidID3Frame( &zero ) );
+  test( !Mp3BaseTagData::IsValidID3Frame( badFrameHdr ) );
+  test( !Mp3BaseTagData::IsValidID3FrameID( "tooLong" ) );
+  test( !Mp3BaseTagData::IsValidID3FrameID( "sm" ) );
+  test( Mp3BaseTagData::IsValidID3FrameID( "TPE1" ) );
 
-  test( Mp3BaseTagData::IsTextFrame( Mp3FrameType::Artist ) );
-  test( Mp3BaseTagData::IsTextFrame( std::string( "TCOM" ) ) );
-  test( Mp3BaseTagData::IsTextFrame( "TALB" ) );
-  test( Mp3BaseTagData::GetFrameType( "TCON" ) == Mp3FrameType::Genre );
-  test( Mp3BaseTagData::GetFrameType( "TCon" ) == Mp3FrameType::None );
+  test( Mp3BaseTagData::IsID3TextFrame( Mp3FrameType::ID3Artist ) );
+  test( Mp3BaseTagData::IsID3TextFrame( std::string( "TCOM" ) ) );
+  test( Mp3BaseTagData::IsID3TextFrame( "TALB" ) );
+  test( Mp3BaseTagData::GetID3FrameType( "TCON" ) == Mp3FrameType::ID3Genre );
+  test( Mp3BaseTagData::GetID3FrameType( "TCon" ) == Mp3FrameType::None );
 }
 
 void TestTagData()
@@ -63,23 +63,23 @@ void TestTagData()
   fs::path mp3( "MP3\\test.mp3" );
   Mp3TagData tag;
   test( tag.LoadTagData( mp3 ) );
-  test( tag.GetText( Mp3FrameType::Title ) == "Title" );
-  test( tag.GetText( Mp3FrameType::Subtitle ) == "" );
-  test( tag.GetText( Mp3FrameType::Genre ) == "Genre" );
-  test( tag.GetText( Mp3FrameType::Artist ) == "Artist" );
-  test( tag.GetText( Mp3FrameType::Album ) == "Album" );
-  test( tag.GetText( Mp3FrameType::Composer ) == "Composer" );
-  test( tag.GetText( Mp3FrameType::Orchestra ) == "Orchestra" );
-  test( tag.GetText( Mp3FrameType::OrigArtist ) == "OrigArtist" );
-  test( tag.GetText( Mp3FrameType::Year ) == "1234" );
-  test( tag.GetText( Mp3FrameType::OrigYear ) == "4321" );
-  test( tag.GetText( Mp3FrameType::TrackNum ) == "333" );
-  test( tag.GetText( Mp3FrameType::BeatsPerMinute ) == "123" );
-  test( tag.GetText( Mp3FrameType::Duration ) == "219167" );
-  test( tag.GetText( Mp3FrameType::Key ) == "C#m" );
-  test( tag.GetText( Mp3FrameType::Conductor ) == "Conductor" );
-  test( tag.GetText( Mp3FrameType::Language ) == "eng" );
-  test( tag.GetText( Mp3FrameType::Mood ) == "Wild" );
+  test( tag.GetText( Mp3FrameType::ID3Title ) == "Title" );
+  test( tag.GetText( Mp3FrameType::ID3Subtitle ) == "" );
+  test( tag.GetText( Mp3FrameType::ID3Genre ) == "Genre" );
+  test( tag.GetText( Mp3FrameType::ID3Artist ) == "Artist" );
+  test( tag.GetText( Mp3FrameType::ID3Album ) == "Album" );
+  test( tag.GetText( Mp3FrameType::ID3Composer ) == "Composer" );
+  test( tag.GetText( Mp3FrameType::ID3Orchestra ) == "Orchestra" );
+  test( tag.GetText( Mp3FrameType::ID3OrigArtist ) == "OrigArtist" );
+  test( tag.GetText( Mp3FrameType::ID3Year ) == "1234" );
+  test( tag.GetText( Mp3FrameType::ID3OrigYear ) == "4321" );
+  test( tag.GetText( Mp3FrameType::ID3TrackNum ) == "333" );
+  test( tag.GetText( Mp3FrameType::ID3BeatsPerMinute ) == "123" );
+  test( tag.GetText( Mp3FrameType::ID3Duration ) == "219167" );
+  test( tag.GetText( Mp3FrameType::ID3Key ) == "C#m" );
+  test( tag.GetText( Mp3FrameType::ID3Conductor ) == "Conductor" );
+  test( tag.GetText( Mp3FrameType::ID3Language ) == "eng" );
+  test( tag.GetText( Mp3FrameType::ID3Mood ) == "Wild" );
   test( tag.GetCommentCount() == 2 );
   test( tag.GetComment( 0 ) == "Comment" );
   test( tag.GetComment( 1 ) == "Wild" );
@@ -93,56 +93,56 @@ void TestTagData()
   test( tagCopy.LoadTagData( mp3Copy ) );
   for( Mp3FrameType frameType = Mp3FrameType::None; frameType != Mp3FrameType::Max; ++frameType )
   {
-    if( Mp3BaseTagData::IsTextFrame( frameType ) )
+    if( Mp3BaseTagData::IsID3TextFrame( frameType ) )
       test( tag.GetText( frameType ) == tagCopy.GetText( frameType ) );
   }
   test( tag.GetComment( 0 ) == tagCopy.GetComment( 0 ) );
   test( !tagCopy.Write() );
 
   // Modify the copy; some strings are shorter, some longer, some unchanged
-  tagCopy.SetText( Mp3FrameType::Title, "T" );
+  tagCopy.SetText( Mp3FrameType::ID3Title, "T" );
   test( tagCopy.IsDirty() );
-  tagCopy.SetText( Mp3FrameType::Subtitle, "NewSubtitle" );
-  tagCopy.SetText( Mp3FrameType::Genre, "GenreCopy" );
-  tagCopy.SetText( Mp3FrameType::Artist, "ArtistCopy" );
-  tagCopy.SetText( Mp3FrameType::Album, "AlbumCopy" );
-  tagCopy.SetText( Mp3FrameType::Composer, "Comp" );
-  tagCopy.SetText( Mp3FrameType::Orchestra, "Orch" );
-  tagCopy.SetText( Mp3FrameType::OrigArtist, "OrigArt" );
-  tagCopy.SetText( Mp3FrameType::Year, "1234" );
-  tagCopy.SetText( Mp3FrameType::TrackNum, "15/325" );
-  tagCopy.SetText( Mp3FrameType::BeatsPerMinute, "1" );
-  tagCopy.SetText( Mp3FrameType::Duration, "9923821" );
-  tagCopy.SetText( Mp3FrameType::Conductor, "Cond" );
-  tagCopy.SetText( Mp3FrameType::Language, "est" );
-  tagCopy.SetText( Mp3FrameType::Mood, "Crazy" );
+  tagCopy.SetText( Mp3FrameType::ID3Subtitle, "NewSubtitle" );
+  tagCopy.SetText( Mp3FrameType::ID3Genre, "GenreCopy" );
+  tagCopy.SetText( Mp3FrameType::ID3Artist, "ArtistCopy" );
+  tagCopy.SetText( Mp3FrameType::ID3Album, "AlbumCopy" );
+  tagCopy.SetText( Mp3FrameType::ID3Composer, "Comp" );
+  tagCopy.SetText( Mp3FrameType::ID3Orchestra, "Orch" );
+  tagCopy.SetText( Mp3FrameType::ID3OrigArtist, "OrigArt" );
+  tagCopy.SetText( Mp3FrameType::ID3Year, "1234" );
+  tagCopy.SetText( Mp3FrameType::ID3TrackNum, "15/325" );
+  tagCopy.SetText( Mp3FrameType::ID3BeatsPerMinute, "1" );
+  tagCopy.SetText( Mp3FrameType::ID3Duration, "9923821" );
+  tagCopy.SetText( Mp3FrameType::ID3Conductor, "Cond" );
+  tagCopy.SetText( Mp3FrameType::ID3Language, "est" );
+  tagCopy.SetText( Mp3FrameType::ID3Mood, "Crazy" );
 
-  test( tagCopy.GetText( Mp3FrameType::Title ) == "T" );
-  test( tagCopy.GetText( Mp3FrameType::Subtitle ) == "NewSubtitle" );
-  test( tagCopy.GetText( Mp3FrameType::Genre ) == "GenreCopy" );
-  test( tagCopy.GetText( Mp3FrameType::Artist ) == "ArtistCopy" );
-  test( tagCopy.GetText( Mp3FrameType::Album ) == "AlbumCopy" );
-  test( tagCopy.GetText( Mp3FrameType::Composer ) == "Comp" );
-  test( tagCopy.GetText( Mp3FrameType::Orchestra ) == "Orch" );
-  test( tagCopy.GetText( Mp3FrameType::OrigArtist ) == "OrigArt" );
-  test( tagCopy.GetText( Mp3FrameType::Year ) == "1234" );
-  test( tagCopy.GetText( Mp3FrameType::OrigYear ) == "4321" );
-  test( tagCopy.GetText( Mp3FrameType::TrackNum ) == "15/325" );
-  test( tagCopy.GetText( Mp3FrameType::BeatsPerMinute ) == "1" );
-  test( tagCopy.GetText( Mp3FrameType::Duration ) == "9923821" );
-  test( tagCopy.GetText( Mp3FrameType::Key ) == "C#m" );
-  test( tagCopy.GetText( Mp3FrameType::Conductor ) == "Cond" );
-  test( tagCopy.GetText( Mp3FrameType::Language ) == "est" );
-  test( tagCopy.GetText( Mp3FrameType::Mood ) == "Crazy" );
+  test( tagCopy.GetText( Mp3FrameType::ID3Title ) == "T" );
+  test( tagCopy.GetText( Mp3FrameType::ID3Subtitle ) == "NewSubtitle" );
+  test( tagCopy.GetText( Mp3FrameType::ID3Genre ) == "GenreCopy" );
+  test( tagCopy.GetText( Mp3FrameType::ID3Artist ) == "ArtistCopy" );
+  test( tagCopy.GetText( Mp3FrameType::ID3Album ) == "AlbumCopy" );
+  test( tagCopy.GetText( Mp3FrameType::ID3Composer ) == "Comp" );
+  test( tagCopy.GetText( Mp3FrameType::ID3Orchestra ) == "Orch" );
+  test( tagCopy.GetText( Mp3FrameType::ID3OrigArtist ) == "OrigArt" );
+  test( tagCopy.GetText( Mp3FrameType::ID3Year ) == "1234" );
+  test( tagCopy.GetText( Mp3FrameType::ID3OrigYear ) == "4321" );
+  test( tagCopy.GetText( Mp3FrameType::ID3TrackNum ) == "15/325" );
+  test( tagCopy.GetText( Mp3FrameType::ID3BeatsPerMinute ) == "1" );
+  test( tagCopy.GetText( Mp3FrameType::ID3Duration ) == "9923821" );
+  test( tagCopy.GetText( Mp3FrameType::ID3Key ) == "C#m" );
+  test( tagCopy.GetText( Mp3FrameType::ID3Conductor ) == "Cond" );
+  test( tagCopy.GetText( Mp3FrameType::ID3Language ) == "est" );
+  test( tagCopy.GetText( Mp3FrameType::ID3Mood ) == "Crazy" );
 
   tagCopy.SetComment( 0, "CommentCopy" );
   test( tagCopy.GetComment( 0 ) == "CommentCopy" );
 
   // Nuke some existing and non-existent frames
-  tagCopy.SetText( Mp3FrameType::OrigArtist, "" );
-  tagCopy.SetText( Mp3FrameType::OrigArtist, "" );
-  tagCopy.SetText( Mp3FrameType::OrigArtist, "OrigArtist" );
-  tagCopy.SetText( Mp3FrameType::BeatsPerMinute, "" );
+  tagCopy.SetText( Mp3FrameType::ID3OrigArtist, "" );
+  tagCopy.SetText( Mp3FrameType::ID3OrigArtist, "" );
+  tagCopy.SetText( Mp3FrameType::ID3OrigArtist, "OrigArtist" );
+  tagCopy.SetText( Mp3FrameType::ID3BeatsPerMinute, "" );
   tagCopy.SetComment( 0, "CommentCopy" );
   tagCopy.SetComment( 1, "" );
   tagCopy.SetComment( 1, "RestoreComment1" );
@@ -156,13 +156,13 @@ void TestTagData()
   test( tagRead.LoadTagData( mp3Copy ) );
   for( Mp3FrameType frameType = Mp3FrameType::None; frameType != Mp3FrameType::Max; ++frameType )
   {
-    if( Mp3BaseTagData::IsTextFrame( frameType ) )
+    if( Mp3BaseTagData::IsID3TextFrame( frameType ) )
     {
       switch( frameType )
       {
-      case Mp3FrameType::OrigArtist:     test( tagRead.GetText( frameType ) == "OrigArtist" ); break;
-      case Mp3FrameType::BeatsPerMinute: test( tagRead.GetText( frameType ) == "" ); break;
-      default:                           test( tagRead.GetText( frameType ) == tagCopy.GetText( frameType ) ); break;
+      case Mp3FrameType::ID3OrigArtist:     test( tagRead.GetText( frameType ) == "OrigArtist" ); break;
+      case Mp3FrameType::ID3BeatsPerMinute: test( tagRead.GetText( frameType ) == "" ); break;
+      default:                              test( tagRead.GetText( frameType ) == tagCopy.GetText( frameType ) ); break;
       }
     }
   }
@@ -182,7 +182,7 @@ void TestTagData()
   test( tagRead2.LoadTagData( mp3Copy ) );
   for( Mp3FrameType frameType = Mp3FrameType::None; frameType != Mp3FrameType::Max; ++frameType )
   {
-    if( Mp3BaseTagData::IsTextFrame( frameType ) )
+    if( Mp3BaseTagData::IsID3TextFrame( frameType ) )
       test( tagRead2.GetText( frameType ) == tagRead.GetText( frameType ) );
   }
   test( tagRead2.GetComment( 0 ) == huge );
@@ -288,7 +288,7 @@ void ForEachSong::operator()( const fs::path& path ) const
   test( tagRead.LoadTagData( fileCopy ) );
   for( Mp3FrameType frameType = Mp3FrameType::None; frameType != Mp3FrameType::Max; ++frameType )
   {
-    if( Mp3BaseTagData::IsTextFrame( frameType ) )
+    if( Mp3BaseTagData::IsID3TextFrame( frameType ) )
     {
       if( frameType == frameType_ )
       {
@@ -316,7 +316,7 @@ void ForEachSong::operator()( const fs::path& path ) const
   test( !tag.IsDirty() );
   test( File( fileCopy ).Delete() ); // comment out this line to leave the temporary files behind for further investigation
   ++frameType_;
-  if( frameType_ == Mp3FrameType::Comment )
+  if( frameType_ == Mp3FrameType::ID3Comment )
   {
     frameType_ = Mp3FrameType::None; // wrap
     ++frameType_;
